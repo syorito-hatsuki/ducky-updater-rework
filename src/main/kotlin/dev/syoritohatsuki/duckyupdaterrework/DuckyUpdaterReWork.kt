d4f233f4f9214a5327b5e9f4d51ca99491b52b50
syorito-hatsuki <enderdron3@gmail.com>
2024-01-14 21:51:57 +0200

    Test run both API layers (Success, but require error handling)

--- src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/DuckyUpdaterReWork.kt
+++ src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/DuckyUpdaterReWork.kt
@@ -1,12 +1,24 @@
 package dev.syoritohatsuki.duckyupdaterrework
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import dev.syoritohatsuki.duckyupdaterrework.core.CommonDuckyUpdaterApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import net.fabricmc.api.ModInitializer
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 
-object DuckyUpdaterReWork {
+object DuckyUpdaterReWork : ModInitializer {
 
     const val MOD_ID = "ducky-updater-rework"
 
-    val logger: Logger = LoggerFactory.getLogger(javaClass.simpleName)
+    val logger: Logger = LogManager.getLogger()
 
+    override fun onInitialize() {
+        CoroutineScope(Dispatchers.IO).launch {
+            CommonDuckyUpdaterApi.getUpdate("fstats-api") {
+                logger.info(it?.changelog ?: "Is null :(")
+            }
+        }
+    }
 }
