75d7ba9fe9a631d0f1babcfb2def68494017421c
syorito-hatsuki <enderdron3@gmail.com>
2024-01-14 21:48:30 +0200

    Extracted hashing tool from legacy code (Require refactor)

---
+++ src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/util/Hash.kt
@@ -0,0 +1,27 @@
+package dev.syoritohatsuki.duckyupdaterrework.util
+
+import com.google.common.hash.Hashing
+import com.google.common.io.Files
+import net.fabricmc.loader.api.FabricLoader
+import net.fabricmc.loader.api.metadata.ModOrigin
+import java.io.IOException
+import kotlin.jvm.optionals.getOrNull
+
+object Hash {
+    fun getSha512Hash(modId: String): String? {
+        FabricLoader.getInstance().getModContainer(modId).getOrNull()?.let { container ->
+            if (container.containingMod.isEmpty && container.origin.kind == ModOrigin.Kind.PATH) container.origin.paths.stream()
+                .filter { path ->
+                    path.toString().lowercase().endsWith(".jar")
+                }.findFirst().getOrNull()?.toFile()?.let {
+                    if (it.isFile) try {
+                        return Files.asByteSource(it).hash(Hashing.sha512()).toString()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+        }
+
+        return null
+    }
+}
