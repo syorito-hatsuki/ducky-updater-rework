a37899e16f4795c24e3e2e853aadf3221fe3314a
syorito-hatsuki <enderdron3@gmail.com>
2024-01-14 21:51:02 +0200

    Created client side entrypoint (I don't know why :D)

---
+++ src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/client/DuckyUpdaterReWorkClient.kt
@@ -0,0 +1,13 @@
+package dev.syoritohatsuki.duckyupdaterrework.client
+
+import net.fabricmc.api.ClientModInitializer
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+object DuckyUpdaterReWorkClient : ClientModInitializer {
+
+    val logger: Logger = LogManager.getLogger()
+
+    override fun onInitializeClient() {
+    }
+}
