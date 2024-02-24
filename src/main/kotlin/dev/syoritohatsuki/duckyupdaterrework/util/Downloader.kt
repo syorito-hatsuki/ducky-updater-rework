63f73dda710e5ae3faa2b998c606b121e1cb39f7
syorito-hatsuki <enderdron3@gmail.com>
2024-02-24 20:06:58 +0200

    Stated implement StateMachine to Downloader

--- src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/util/Downloader.kt
+++ src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/util/Downloader.kt
@@ -8,6 +8,20 @@ import java.nio.file.Files
 
 object Downloader {
 
+    enum class State {
+        RUNNING, STOPPED, IDLE
+    }
+
+    enum class Status {
+        COMPLETE, DOWNLOADING, FAILURE
+    }
+
+    enum class Event {
+        START, STOP
+    }
+
+    private var currentState: State = State.IDLE
+
     suspend fun testProgressBar() {
         val urls = listOf(
             "https://cdn.modrinth.com/data/3rc31Hgo/versions/Pg5efHEv/spectrum-1.7.4-deeper-down.jar",
@@ -29,49 +43,50 @@ object Downloader {
             jobs.awaitAll()
         }
     }
-}
 
-suspend fun downloadFileWithProgress(url: String, outputFile: String) {
-    val connection = withContext(Dispatchers.IO) {
-        URL(url).openConnection()
-    }
-    val contentLength = connection.contentLengthLong
-    if (contentLength == -1L) {
-        println("Content length of the file is unknown. Cannot display progress.")
-        return
-    }
-
-    val channel = Channel<Int>()
-    val progressBar = CoroutineScope(Dispatchers.IO).launch {
-        val progressIndicator = ProgressBar(contentLength.toInt())
-        for (progress in channel) {
-            print("\r${outputFile} => [${progressIndicator.getProgressString(progress)}]")
+    private suspend fun downloadFileWithProgress(url: String, outputFile: String) {
+        val connection = withContext(Dispatchers.IO) {
+            URL(url).openConnection()
         }
-        println()
-    }
-
-    val job = CoroutineScope(Dispatchers.IO).launch {
-        val inputStream = connection.getInputStream()
-        val outputStream = Files.newOutputStream(FabricLoader.getInstance().gameDir.resolve("mods").resolve(outputFile))
-        val buffer = ByteArray(1024)
-        var bytesCount: Long = 0
-        var bytesRead: Int
-        while (inputStream.read(buffer).also { bytesRead = it } != -1) {
-            outputStream.write(buffer, 0, bytesRead)
-            bytesCount += bytesRead
-            channel.send(bytesCount.toInt())
+        val contentLength = connection.contentLengthLong
+        if (contentLength == -1L) {
+            println("Content length of the file is unknown. Cannot display progress.")
+            return
         }
-        outputStream.close()
-        channel.close()
+
+        val channel = Channel<Int>()
+        val progressBar = CoroutineScope(Dispatchers.IO).launch {
+            val progressIndicator = ProgressBar(contentLength.toInt())
+            for (progress in channel) {
+                print("\r${outputFile} => [${progressIndicator.getProgressString(progress)}]")
+            }
+            println()
+        }
+
+        val job = CoroutineScope(Dispatchers.IO).launch {
+            val inputStream = connection.getInputStream()
+            val outputStream =
+                Files.newOutputStream(FabricLoader.getInstance().gameDir.resolve("mods").resolve(outputFile))
+            val buffer = ByteArray(1024)
+            var bytesCount: Long = 0
+            var bytesRead: Int
+            while (inputStream.read(buffer).also { bytesRead = it } != -1) {
+                outputStream.write(buffer, 0, bytesRead)
+                bytesCount += bytesRead
+                channel.send(bytesCount.toInt())
+            }
+            outputStream.close()
+            channel.close()
+        }
+
+        job.join()
+        progressBar.join()
     }
 
-    job.join()
-    progressBar.join()
-}
-
-class ProgressBar(private val total: Int) {
-    fun getProgressString(progress: Int): String {
-        val currentProgress = (progress.toDouble() / total * 100).toInt()
-        return "$currentProgress%"
+    class ProgressBar(private val total: Int) {
+        fun getProgressString(progress: Int): String {
+            val currentProgress = (progress.toDouble() / total * 100).toInt()
+            return "$currentProgress%"
+        }
     }
 }
