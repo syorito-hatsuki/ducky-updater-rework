be4383a1ab2376cec57f52fede860f9bfd0fa1fa
syorito-hatsuki <enderdron3@gmail.com>
2024-02-24 20:02:09 +0200

    Extend Version data model

--- src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/core/api/models/Version.kt
+++ src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/core/api/models/Version.kt
@@ -7,13 +7,21 @@ import kotlinx.serialization.Serializable
 data class Version(
     val name: String,
     val changelog: String,
-    @SerialName("date_published")
-    val datePublished: String,
+    @SerialName("date_published") val datePublished: String,
+    val dependencies: List<Dependency> = emptyList(),
     val featured: Boolean,
     val files: List<File>,
-    @SerialName("version_number")
-    val versionNumber: String
+    @SerialName("project_id") val projectId: String,
+    @SerialName("version_number") val versionNumber: String
 ) {
+    @Serializable
+    data class Dependency(
+        @SerialName("version_id") val versionId: String? = "",
+        @SerialName("project_id") val projectId: String? = "",
+        @SerialName("file_name") val fileName: String? = "",
+        @SerialName("dependency_type") val dependencyType: String? = ""
+    )
+
     @Serializable
     data class File(
         val url: String
