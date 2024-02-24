5ddf60d87b56f5551e5ac04d76f8db45386d9409
syorito-hatsuki <enderdron3@gmail.com>
2024-02-24 20:06:08 +0200

    Updated commands run based on pseudo ViewModel

--- src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/core/command/UpdateCommand.kt
+++ src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/core/command/UpdateCommand.kt
@@ -3,10 +3,16 @@ package dev.syoritohatsuki.duckyupdaterrework.core.command
 import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.context.CommandContext
 import dev.syoritohatsuki.duckyupdaterrework.DuckyUpdaterReWork
+import dev.syoritohatsuki.duckyupdaterrework.core.DuckyUpdaterApi
 import dev.syoritohatsuki.duckyupdaterrework.core.command.argument.ModsIdsArgumentType
+import dev.syoritohatsuki.duckyupdaterrework.util.Hash
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import net.minecraft.command.CommandSource
 
 object UpdateCommand {
+
     fun update(context: CommandContext<out CommandSource>): Int {
         val modsIds = runCatching {
             ModsIdsArgumentType.getModsIds(context, "modsIds")
@@ -14,19 +20,28 @@ object UpdateCommand {
             StringArgumentType.getString(context, "modsIdsString").split(" ")
         }
 
-        DuckyUpdaterReWork.logger.error("ModsIds")
-        modsIds.forEachIndexed { index, modId ->
-            DuckyUpdaterReWork.logger.error("$index. $modId")
+        CoroutineScope(Dispatchers.IO).launch {
+            DuckyUpdaterReWork.logger.error("ModsIds")
+            DuckyUpdaterApi.checkForUpdates(Hash.getSha512Hashes(modsIds))
+
+            DuckyUpdaterApi.getUpdatesUrls().onEachIndexed { index, entry ->
+                DuckyUpdaterReWork.logger.error("$index. ${entry.key} ${entry.value}")
+            }
         }
 
         return 1
     }
 
     fun updateAll(@Suppress("UNUSED_PARAMETER") context: CommandContext<out CommandSource>): Int {
-        DuckyUpdaterReWork.logger.error("ModsIds")
-        DuckyUpdaterReWork.modsIds.forEachIndexed { index, modId ->
-            DuckyUpdaterReWork.logger.error("$index. $modId")
+
+        CoroutineScope(Dispatchers.IO).launch {
+            DuckyUpdaterApi.checkForUpdates()
+
+            DuckyUpdaterApi.getUpdatesUrls().onEachIndexed { index, entry ->
+                DuckyUpdaterReWork.logger.error("$index. ${entry.key} ${entry.value}")
+            }
         }
+
         return 1
     }
 }
