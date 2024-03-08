b78365ae5bf4c57fba64cd6618f4c4da72698ecd
syorito-hatsuki <enderdron3@gmail.com>
2024-03-08 02:14:46 +0200

    Changed SHA getter output to ModContainer instead of raw id string and removed unused methods

--- src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/util/Hash.kt
+++ src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/util/Hash.kt
@@ -4,37 +4,18 @@ import com.google.common.hash.Hashing
 import com.google.common.io.Files
 import net.fabricmc.loader.api.FabricLoader
 import net.fabricmc.loader.api.ModContainer
+import net.fabricmc.loader.api.metadata.ModMetadata
 import net.fabricmc.loader.api.metadata.ModOrigin
 import java.io.File
 import java.io.IOException
 import kotlin.jvm.optionals.getOrNull
 
 object Hash {
-    fun getSha512Hash(modId: String): String? {
-        FabricLoader.getInstance().getModContainer(modId).getOrNull()?.let { container ->
-            containerToFile(container)?.let { file ->
-                if (file.isFile) return hashFile(file)
-            }
-        }
-
-        return null
-    }
-
-    fun getSha512Hashes(modsIds: List<String>): List<String> = mutableListOf<String>().apply {
-        FabricLoader.getInstance().allMods.filter { modsIds.contains(it.metadata.id) }.forEach { container ->
-            containerToFile(container)?.let { file ->
-                if (file.isFile) hashFile(file)?.let {
-                    add(it)
-                }
-            }
-        }
-    }
-
-    fun getSha512Hashes(): Map<String, String> = mutableMapOf<String, String>().apply {
+    fun getSha512Hashes(): Map<String, ModMetadata> = mutableMapOf<String, ModMetadata>().apply {
         FabricLoader.getInstance().allMods.forEach { container ->
             containerToFile(container)?.let { file ->
                 if (file.isFile) hashFile(file)?.let {
-                    this[it] = container.metadata.id
+                    this[it] = container.metadata
                 }
             }
         }
