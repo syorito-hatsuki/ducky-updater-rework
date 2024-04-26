4f0362a78f2b80b74f05000dd546018668837f63
syorito-hatsuki <enderdron3@gmail.com>
2024-04-26 21:03:34 +0300

    Added @NotNull

--- src/main/java/dev/syoritohatsuki/duckyupdaterrework/mixin/UnlimitedChat.java
+++ src/main/java/dev/syoritohatsuki/duckyupdaterrework/mixin/UnlimitedChat.java
@@ -1,6 +1,7 @@
 package dev.syoritohatsuki.duckyupdaterrework.mixin;
 
 import net.minecraft.client.gui.hud.ChatHud;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,12 +10,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ChatHud.class)
 public abstract class UnlimitedChat {
     @Inject(method = "getWidth()I", at = @At("HEAD"), cancellable = true)
-    private void unlimitedChatWidth(CallbackInfoReturnable<Integer> cir) {
+    private void unlimitedChatWidth(@NotNull CallbackInfoReturnable<Integer> cir) {
         cir.setReturnValue(1000);
     }
 
     @Inject(method = "getHeight()I", at = @At("HEAD"), cancellable = true)
-    private void unlimitedChatHeight(CallbackInfoReturnable<Integer> cir) {
+    private void unlimitedChatHeight(@NotNull CallbackInfoReturnable<Integer> cir) {
         cir.setReturnValue(1000);
     }
 }
