fb5e390d15a66143bd78fc79aac860b4fe599098
syorito-hatsuki <enderdron3@gmail.com>
2024-05-03 23:25:08 +0300

    Added OP requirement for server commands and on boot check

--- src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/server/DuckyUpdaterReWorkServer.kt
+++ src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/server/DuckyUpdaterReWorkServer.kt
@@ -2,10 +2,12 @@ package dev.syoritohatsuki.duckyupdaterrework.server
 
 import dev.syoritohatsuki.duckyupdaterrework.DuckyUpdaterReWork.logger
 import dev.syoritohatsuki.duckyupdaterrework.core.command.commands
+import dev.syoritohatsuki.duckyupdaterrework.core.config.ConfigManager
 import dev.syoritohatsuki.duckyupdaterrework.core.dsl.register
 import dev.syoritohatsuki.duckyupdaterrework.core.dsl.rootLiteral
 import net.fabricmc.api.DedicatedServerModInitializer
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
 
 object DuckyUpdaterReWorkServer : DedicatedServerModInitializer {
     override fun onInitializeServer() {
@@ -14,8 +16,16 @@ object DuckyUpdaterReWorkServer : DedicatedServerModInitializer {
         CommandRegistrationCallback.EVENT.register { dispatcher, _, _ ->
             dispatcher.register {
                 rootLiteral("durw-server") {
-                    commands()
+                    requires {
+                        it.hasPermissionLevel(4)
+                    }.commands()
                 }
+            }
+        }
+
+        ServerLifecycleEvents.SERVER_STARTED.register {
+            if (ConfigManager.read().checkUpdatesOnBoot) {
+                it.commandManager.dispatcher.execute("durw-server check", it.commandSource)
             }
         }
     }
