bc6f20a6b0ae0fb43794f23a08b78490dfd0c3b6
syorito-hatsuki <enderdron3@gmail.com>
2024-05-04 01:25:49 +0300

    How I lost that padding...

--- src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/client/message/UpdateAvailable.kt
+++ src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/client/message/UpdateAvailable.kt
@@ -10,27 +10,27 @@ import net.minecraft.util.Formatting
 
 fun updateAvailable(printer: Printer, additionalInfos: MutableMap<ProjectId, AdditionalInfo>): Text =
     Text.empty().apply {
-    append(Text.literal(printer.prefix))
-    append(Text.literal(" [").formatted(Formatting.DARK_GRAY))
-    if (printer.currentExist) {
+        append(Text.literal(printer.prefix))
+        append(Text.literal(" [").formatted(Formatting.DARK_GRAY))
+        if (printer.currentExist) {
+            append(Text.literal(printer.matchedVersion).formatted(Formatting.GRAY))
+            append(Text.literal(printer.currentUnMatchVersion).formatted(Formatting.RED))
+            append(Text.literal(" -> ").formatted(Formatting.DARK_GRAY))
+        }
         append(Text.literal(printer.matchedVersion).formatted(Formatting.GRAY))
-        append(Text.literal(printer.currentUnMatchVersion).formatted(Formatting.RED))
-        append(Text.literal(" -> ").formatted(Formatting.DARK_GRAY))
-    }
-    append(Text.literal(printer.matchedVersion).formatted(Formatting.GRAY))
-    append(Text.literal(printer.newUnMatchVersion).formatted(Formatting.GREEN))
-    append(Text.literal("]").formatted(Formatting.DARK_GRAY))
-    styled { style ->
-        style.withClickEvent(
-            ClickEvent(
-                ClickEvent.Action.SUGGEST_COMMAND, "/durw-client update ${printer.projectId}"
-            )
-        ).withHoverEvent(
-            HoverEvent(
-                HoverEvent.Action.SHOW_TEXT, Text.literal(
-                    additionalInfos[printer.projectId]?.changeLog ?: return@styled style
+        append(Text.literal(printer.newUnMatchVersion).formatted(Formatting.GREEN))
+        append(Text.literal("]").formatted(Formatting.DARK_GRAY))
+        styled { style ->
+            style.withClickEvent(
+                ClickEvent(
+                    ClickEvent.Action.SUGGEST_COMMAND, "/durw-client update ${printer.projectId}"
+                )
+            ).withHoverEvent(
+                HoverEvent(
+                    HoverEvent.Action.SHOW_TEXT, Text.literal(
+                        additionalInfos[printer.projectId]?.changeLog ?: return@styled style
+                    )
                 )
             )
-        )
+        }
     }
-}
