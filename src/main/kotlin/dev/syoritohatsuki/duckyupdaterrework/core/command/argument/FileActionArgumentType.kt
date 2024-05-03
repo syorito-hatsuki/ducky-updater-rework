171c80b5049bb550e1c0fff804b19bc5ee4bc089
syorito-hatsuki <enderdron3@gmail.com>
2024-05-03 23:23:36 +0300

    Changed package from Downloader to FileAction

--- src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/core/command/argument/FileActionArgumentType.kt
+++ src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/core/command/argument/FileActionArgumentType.kt
@@ -5,31 +5,31 @@ import com.mojang.brigadier.arguments.ArgumentType
 import com.mojang.brigadier.context.CommandContext
 import com.mojang.brigadier.suggestion.Suggestions
 import com.mojang.brigadier.suggestion.SuggestionsBuilder
-import dev.syoritohatsuki.duckyupdaterrework.core.util.Downloader
+import dev.syoritohatsuki.duckyupdaterrework.core.util.FileActions
 import net.minecraft.command.CommandSource
 import java.util.concurrent.CompletableFuture
 
-class FileActionArgumentType : ArgumentType<Downloader.FileAction> {
+class FileActionArgumentType : ArgumentType<FileActions.FileAction> {
 
     companion object {
         fun fileAction(): FileActionArgumentType = FileActionArgumentType()
 
-        fun getFileAction(context: CommandContext<out CommandSource>, argumentName: String): Downloader.FileAction =
-            context.getArgument(argumentName, Downloader.FileAction::class.java)
+        fun getFileAction(context: CommandContext<out CommandSource>, argumentName: String): FileActions.FileAction =
+            context.getArgument(argumentName, FileActions.FileAction::class.java)
     }
 
     override fun <S> listSuggestions(
         context: CommandContext<S>, builder: SuggestionsBuilder
     ): CompletableFuture<Suggestions> =
-        CommandSource.suggestMatching(Downloader.FileAction.values().map { it.name }, builder) ?: Suggestions.empty()
+        CommandSource.suggestMatching(FileActions.FileAction.values().map { it.name }, builder) ?: Suggestions.empty()
 
-    override fun parse(reader: StringReader): Downloader.FileAction {
+    override fun parse(reader: StringReader): FileActions.FileAction {
         val cursor = reader.cursor
 
         while (reader.canRead()) reader.skip()
 
-        return Downloader.FileAction.valueOf(reader.string.substring(cursor, reader.cursor))
+        return FileActions.FileAction.valueOf(reader.string.substring(cursor, reader.cursor))
     }
 
-    override fun getExamples(): Collection<String> = Downloader.FileAction.values().map { it.name }
+    override fun getExamples(): Collection<String> = FileActions.FileAction.values().map { it.name }
 }
