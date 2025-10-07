a67054a004b1b46426f57e69428624263a41b5d6
syorito-hatsuki <enderdron3@gmail.com>
2025-10-08 00:53:20 +0300

    Fixed thread issue on 1.21.9

--- src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/core/util/Extensions.kt
+++ src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/core/util/Extensions.kt
@@ -1,7 +1,11 @@
 package dev.syoritohatsuki.duckyupdaterrework.core.util
 
 import dev.syoritohatsuki.duckyupdaterrework.DuckyUpdaterReWork.logger
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.launch
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
+import net.minecraft.client.MinecraftClient
 import net.minecraft.command.CommandSource
 import net.minecraft.server.command.ServerCommandSource
 import net.minecraft.text.Text
@@ -11,7 +15,11 @@ fun Boolean.toInt(): Int = if (this) 1 else 0
 fun CommandSource.sendMessage(vararg textLines: Text) {
     when (this) {
         is ServerCommandSource -> textLines.map { player?.sendMessage(it, false) }
-        is FabricClientCommandSource -> textLines.map(::sendFeedback)
+        is FabricClientCommandSource -> textLines.map {
+            CoroutineScope(MinecraftClient.getInstance().asCoroutineDispatcher()).launch {
+                sendFeedback(it)
+            }
+        }
     }
 }
 
