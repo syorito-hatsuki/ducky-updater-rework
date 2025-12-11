d1b98570af8ef4a46f1b1c2a8629b7f77df1130f
Collin <104375741+qtqgyt@users.noreply.github.com>
2025-12-11 16:50:16 +0000

    A very, very cursed update to 1.21.11.

--- src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/server/DuckyUpdaterReWorkServer.kt
+++ src/main/kotlin/dev/syoritohatsuki/duckyupdaterrework/server/DuckyUpdaterReWorkServer.kt
@@ -3,12 +3,12 @@ package dev.syoritohatsuki.duckyupdaterrework.server
 import dev.syoritohatsuki.duckyupdaterrework.DuckyUpdaterReWork.logger
 import dev.syoritohatsuki.duckyupdaterrework.core.DuckyUpdaterApi
 import dev.syoritohatsuki.duckyupdaterrework.core.command.commands
-import dev.syoritohatsuki.duckyupdaterrework.core.config.ConfigManager
 import dev.syoritohatsuki.duckyupdaterrework.core.dsl.register
 import dev.syoritohatsuki.duckyupdaterrework.core.dsl.rootLiteral
 import net.fabricmc.api.DedicatedServerModInitializer
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
+import net.minecraft.server.command.ServerCommandSource
 import net.minecraft.util.WorldSavePath
 
 object DuckyUpdaterReWorkServer : DedicatedServerModInitializer {
@@ -18,21 +18,13 @@ object DuckyUpdaterReWorkServer : DedicatedServerModInitializer {
         CommandRegistrationCallback.EVENT.register { dispatcher, _, _ ->
             dispatcher.register {
                 rootLiteral("durw-server") {
-                    requires {
-                        it.hasPermissionLevel(4)
-                    }.commands()
+                    requires { it is ServerCommandSource }.commands()
                 }
             }
         }
 
         ServerLifecycleEvents.SERVER_STARTING.register {
             DuckyUpdaterApi.defaultDatapacksDir = it.getSavePath(WorldSavePath.DATAPACKS)
-        }
-
-        ServerLifecycleEvents.SERVER_STARTED.register {
-            if (ConfigManager.read().checkUpdatesOnBoot) {
-                it.commandManager.dispatcher.execute("durw-server check fabric", it.commandSource)
-            }
         }
     }
 }
